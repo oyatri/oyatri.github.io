5cba067e265a4b6233dbb4446f7908d0704f5422
oyatri <crucio8467@gmail.com>
2022-01-10 23:08:09 +0900

    custom plugin

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "[ Jekyll ] Main 화면 Sidebar에 category 출력",
+        "excerpt":"진행 1 : _data/navigation.yml 파일에 Sidebar category 로 쓰기 위해 \"docs\" 로 명명한 코드를 작성한다. 2 : index.html 파일에서 빨간박스 안의 내용을 추가한다. nav: 에는 1: 에서 작성해준 \"docs\"를 입력한다. 결과 코드 index.html --- layout: home author_profile: true sidebar: nav: \"docs\" --- _data&gt;navigation.yml # Sidebar docs: - title: \"FRAMEWORK\" children:...","categories": ["Jekyll"],
+        "tags": ["minimal mistakes","blog","github","jekyll"],
+        "url": "/jekyll/main_sidebar/",
+        "teaser": null
+      },{
+        "title": "[ Jekyll ] Posting test",
+        "excerpt":"2022-01-10 23:02:39 +0900  2022-01-10T23:02:39+09:00  ","categories": ["Jekyll"],
+        "tags": [],
+        "url": "/jekyll/test/",
+        "teaser": null
+      },{
+        "title": "[ Jekyll ] minimal-stakes 포스트 리스트 제목 밑줄 제거",
+        "excerpt":"수정 전 밑줄이 왠지 거슬린다. 진행 1 : _sass/minimal-mistakes/_base.scss 파일의 빨간 박스 부분에 해당 코드를 작성한다. 결과 밑줄이 사라진 것을 확인할 수 있다. 코드 /* links */ a { /* a link 하이퍼링크 밑줄 없애기 */ text-decoration: none; &amp;:focus { @extend %tab-focus; } &amp;:visited { color: $link-color-visited; } &amp;:hover {...","categories": ["Jekyll"],
+        "tags": [],
+        "url": "/jekyll/delTitleUderline/",
+        "teaser": null
+      }]
